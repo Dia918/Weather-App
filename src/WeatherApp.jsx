b7961e0a911d0a1841ceee9ae0079a5a784ce781
Dia Negi <negidia918@gmail.com>
2025-07-17 09:58:52 +0530

    Update WeatherApp.jsx

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -20,15 +20,7 @@ export default function WeatherApp() {
     };
 
     return(
-        <div style={{ textAlign: "center", 
-            backgroundImage: 'url(https://th.bing.com/th/id/R.069cf185a7daba4caceb8e10e84da46b?rik=H6UDZfO%2b56isFg&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f07%2fDownload-Free-Weather-Background.jpg&ehk=JNt8y9ofXKAnT7OCpITBYQSnexCUjI9Hmcb24SZmQrs%3d&risl=&pid=ImgRaw&r=0)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            position:'fixed',
-            top:0,
-            left: 0,
-            height: '100vh',
-            width: '100vw'
+        <div style={{ textAlign: "center"
         }}>
         <h2>Weather App</h2>
         <SearchBox updateInfo={updateInfo} />
